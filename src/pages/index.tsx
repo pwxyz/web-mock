8353f965ed8b3013e8f438df76735c946a4da532
pengwei <pengwei@antiy.cn>
2019-12-10 11:31:00 +0800

    添加 project的 curd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from './index.css';
+import styles from './index.less';
 import request from '@/utils/request';
 import api from '@/constants/api'
 import dayjs from 'dayjs'
 import router from 'umi/router'
 import { connect } from 'dva';
-import { Button, Form, Input, Modal, message } from 'antd';
+import { Button, Form, Input, Modal, message, Popconfirm, Switch } from 'antd';
 import result from 'lodash/result'
 
 interface InitState {
@@ -13,6 +13,8 @@ interface InitState {
     { name: string, createdAt: number, _id: string }
   ] | [],
   modalVisible: boolean,
+  id?: string,
+  selectArg?: object
   isNew: boolean
 }
 
@@ -33,6 +35,8 @@ class IndexPage extends React.Component<any, any>{
   state: InitState = {
     data: [],
     modalVisible: false,
+    id: '',
+    selectArg: {},
     isNew: true
   }
   componentDidMount() {
@@ -55,32 +59,51 @@ class IndexPage extends React.Component<any, any>{
     }
   }
 
+  delProject = async (id: string) => {
+    let { err } = await request({ method: 'delete', url: api.PROJECT, data: { id: [id] } })
+    if (!err) {
+      message.success('删除成功')
+      this.getProject()
+    }
+  }
+
+  editProject = async (obj: object) => {
+    let data = { ...obj, id: this.state.id }
+    let { err } = await request({ method: 'put', url: api.PROJECT, data })
+    if (!err) {
+      message.success('修改成功')
+      this.getProject()
+    }
+  }
+
   projectClick = async (id: string) => {
     router.push(`/project/${id}`)
   }
 
   showProjectModal = (id?: string) => {
-    this.setState({ modalVisible: true, isNew: !id })
+    let selectArg = result(this.state, 'data', []).find(i => i['_id'] === id) || {}
+    this.setState({ modalVisible: true, isNew: !id, id, selectArg })
   }
 
   hideProjectModal = () => {
     this.setState({ modalVisible: false })
   }
 
+
   render() {
-    const { data, isNew, modalVisible } = this.state
+    const { data, isNew, modalVisible, selectArg } = this.state
     // console.log(this.props)
     const { isLogin = false } = this.props
     const add = () => this.showProjectModal()
     return (
       <div className={styles.container} >
         {
-          data && data.length ? data.map(i => <Project name={i.name} createdAt={i.createdAt} key={i._id} id={i._id} onClick={this.projectClick} />) : '暂无项目'
+          data && data.length ? data.map(i => <Project name={i.name} createdAt={i.createdAt} key={i._id} id={i._id} onClick={this.projectClick} delProject={this.delProject} showEditProject={this.showProjectModal} />) : '暂无项目'
         }
         {isLogin && <div style={{ position: 'fixed', right: 70, top: 70 }} ><Button type='primary' onClick={add} >新增项目</Button></div>}
         <Modal footer={null} visible={modalVisible} maskClosable={false} onCancel={this.hideProjectModal} title={isNew ? '新增' : '编辑'} >
 
-          <ProjectModalContent onChange={this.addProject} />
+          <ProjectModalContent onChange={isNew ? this.addProject : this.editProject} isNew={isNew} data={selectArg} />
         </Modal>
       </div>
     )
@@ -92,16 +115,27 @@ interface ProjectProps {
   createdAt: number,
   id: string,
   onClick: (id: string) => void
+  delProject: (id: string) => void
+  showEditProject: (id: string) => void
 }
 
-const Project = ({ name, createdAt, id, onClick }: ProjectProps) => {
+const Project = ({ name, createdAt, id, onClick, delProject, showEditProject }: ProjectProps) => {
   const getClick = () => {
     onClick(id)
   }
   return (
-    <div className={styles.project} onClick={getClick}  >
-      <div>{name}</div>
-      <div>{dayjs(createdAt).format('YYYY-MM-DD HH:mm:ss')}</div>
+    <div className={styles.project}   >
+      <div onClick={getClick} style={{ cursor: 'pointer' }} >
+        <div>{name}</div>
+        <div>{dayjs(createdAt).format('YYYY-MM-DD HH:mm:ss')}</div>
+      </div>
+      <div className={styles.btncontent} >
+        <Popconfirm title='确定删除该项目?' onConfirm={_ => delProject(id)} >
+          <Button type='danger' >删除</Button>
+        </Popconfirm>
+
+        <Button onClick={_ => showEditProject(id)} >编辑</Button>
+      </div>
     </div>
   )
 }
@@ -147,12 +181,12 @@ class ProjectContent extends React.Component<any, any>{
   }
 
   render() {
-    const { data = {} } = this.props
+    const { data = {}, isNew } = this.props
     const { getFieldDecorator } = this.props.form
     return (
       <div>
         <Form  {...formItemLayout} className={styles.forms}>
-          <FormItem required={false} label='name'>
+          <FormItem required={true} label='name'>
             {getFieldDecorator('name', {
               rules: [{ required: true, message: '请输入name' }],
               initialValue: result(data, 'name') || ''
@@ -160,7 +194,7 @@ class ProjectContent extends React.Component<any, any>{
               <Input />
             )}
           </FormItem>
-          <FormItem required={false} label='description' >
+          <FormItem required={true} label='description' >
             {getFieldDecorator('description', {
               initialValue: result(data, 'description') || ''
             })(
@@ -169,7 +203,23 @@ class ProjectContent extends React.Component<any, any>{
           </FormItem>
           <FormItem required={false} label='testUrl'>
             {getFieldDecorator('testUrl', {
-              initialValue: result(data, 'testUrl') || ''
+              initialValue: result(data, 'testUrl') || '',
+
+            })(
+              <Input />
+            )}
+          </FormItem>
+          <FormItem required={true} label='是否允许自动添加'>
+            {getFieldDecorator('allowAdd', {
+              initialValue: result(data, 'allowAdd') || true,
+              valuePropName: 'checked'
+            })(
+              <Switch />
+            )}
+          </FormItem>
+          <FormItem required={false} label='routerPrefix'>
+            {getFieldDecorator('routerPrefix', {
+              initialValue: result(data, 'routerPrefix') || ''
             })(
               <Input />
             )}
